61d5b616def6e0a93d91d0fbc163bb74dd139a39
SneekeeStache <kyle.prudhomme@proton.me>
2025-06-04 16:25:12 +0200

    terrain fonctionel avec placement de carte

--- Assets/Scripts/SelectManager.cs
+++ Assets/Scripts/SelectManager.cs
@@ -9,29 +9,42 @@ public class SelectManager : NetworkBehaviour
 
     public void selectCard(GameObject card)
     {
+        Debug.Log("click card");
         if (cardSelected != card)
         {
             cardSelected = card;
+            Debug.Log("selected");
         }
         else
         {
             cardSelected = null;
+            Debug.Log("unselected");
         }
     }
 
     public void selectTerrain(GameObject terrain)
     {
+        Debug.Log("click terrain");
         if(cardSelected == null) return;
         NetworkObject monTerrain = terrain.GetComponent<NetworkObject>();
         NetworkObject maCarte = cardSelected.GetComponent<NetworkObject>();
+        Debug.Log(monTerrain.Id.Raw);
         RPC_dropCard(monTerrain.Id,  maCarte.Id);
+        cardSelected = null;
     }
     
-    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPC_dropCard(NetworkId terrainId,NetworkId cardId)
     {
+        Debug.Log("terrain: "+terrainId+"card: "+ cardId);
+        
         Runner.TryFindObject(cardId,out NetworkObject myCard);
         Runner.TryFindObject(terrainId, out NetworkObject terrain);
         myCard.gameObject.transform.SetParent(terrain.gameObject.transform);
+        RectTransform rectCard = myCard.GetComponent<RectTransform>();
+        rectCard.anchorMin = new Vector2(0.5f, 0.5f);
+        rectCard.anchorMax = new Vector2(0.5f, 0.5f);
+        
+
     }
 }
